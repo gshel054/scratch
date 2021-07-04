c601e11c50ae0ab9fb1bdca5402cce6cfcb8d85b
Gary Shelhimer <gshel054@gmail.com>
2021-07-04 18:23:35 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -3,11 +3,12 @@
 # runme.sh
 #
 
-rm -fv /etc/pki/rpm-gpg/*
-rm -fv /etc/yum.repos.d/*
-tar -xvf rpm-gpg.tar.bz2
-tar -xvf yum.repos.d.tar.bz2
-cp -v rpm-gpg/* /etc/pki/rpm-gpg/
-cp -v yum.repos.d/* /etc/yum.repos.d/
+rm -f /etc/pki/rpm-gpg/*
+rm -f /etc/yum.repos.d/*
+tar -xf rpm-gpg.tar.bz2
+tar -xf yum.repos.d.tar.bz2
+cp rpm-gpg/* /etc/pki/rpm-gpg/
+cp yum.repos.d/* /etc/yum.repos.d/
 rm -rf rpm-gpg
 rm -rf yum.repos.d
+echo All Done!
